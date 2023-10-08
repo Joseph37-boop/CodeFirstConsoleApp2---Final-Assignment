a546d4e81d460ed8fcf6313726fdb1637d9477f3
Administrator <Administrator@HPLAPTOP>
2023-10-08 02:34:49 -0600

    Add project files.

---
+++ CodeFirstConsoleApp2/Module1.vb
@@ -0,0 +1,46 @@
+﻿Using System;
+
+Public Class Student
+{
+    Public int StudentId { Get; Set; }
+    Public String Name { Get; Set; }
+    Public DateTime EnrollmentDate { Get; Set; }
+}
+
+Using System.Data.Entity;
+
+Public Class SchoolContext :  DbContext
+{
+    Public DbSet<Student> Students { Get; Set; }
+}
+
+        Class Program
+{
+    Static void Main(String[] args)
+    {
+        Using (var context = New SchoolContext())
+        {
+            // Create the database
+            context.Database.CreateIfNotExists();
+
+            // Add a student
+            var student = New Student
+            {
+                Name = "John Doe",
+                EnrollmentDate = DateTime.Now
+            };
+            context.Students.Add(student);
+            context.SaveChanges();
+        }
+    }
+}
+
+
+
+Module Module1
+
+        Sub Main()
+
+        End Sub
+
+    End Module
